583b0396a035cb164d4c503ad158be6bd02bf8b8
Jeff Woo <cheeseonhead@gmail.com>
2015-12-01 07:37:10 -0500

    Now welcomes the user when he/she's logged in

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,36 @@ var sess;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	sess = req.session;
+	var usersColl = req.db.get('users');
+
 	data = {
 		login: true
 	}
+
 	if (sess['logged_in_as']) {
-		data.login = false
+
+		usersColl.find({
+			username: sess['logged_in_as']
+		}, {}, function(err, users) {
+			if (err) {
+				result['success'] = 0;
+				result['err_type'] = 'mongodb_error';
+				result['err_msg'] = "Unknown Error Occured.";
+				res.send(result);
+			} else if (users.length <= 0) {
+				result['success'] = 0;
+				result['err_type'] = 'user_no_exists';
+				result['err_msg'] = "This username is not registered.";
+				res.send(result);
+			} else {
+				data.firstname = users[0].firstname;
+				data.login = false
+				res.render('index', data);
+			}
+		});
+	} else {
+		res.render('index', data);
 	}
-	res.render('index', data);
 });
 
 module.exports = router;
