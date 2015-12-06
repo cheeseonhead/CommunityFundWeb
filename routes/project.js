c64b52825f49aad55593315aba0e43b331593d0d
Jeff Woo <cheeseonhead@gmail.com>
2015-12-06 12:44:49 -0500

    Added project description

--- routes/project.js
+++ routes/project.js
@@ -82,6 +82,7 @@ router.get('/', function(req, res, next) {
 					data['p_goal'] = projects[0].funding_goal;
 					data['p_reputation'] = projects[0].reputation;
 					data['p_id'] = projects[0].id;
+					data['p_description'] = projects[0].description;
 					p_int_id = projects[0].categories;
 					ownerUsername = projects[0].owner_username;
 					tm.runNext();
